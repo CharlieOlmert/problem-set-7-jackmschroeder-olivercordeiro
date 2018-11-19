4c7a038b45906ac4bf75ee171b280471ed3d5a2d
Jack Schroeder <jackschroeder@The-Leviathan.local>
2018-11-19 00:05:39 -0500

    pre-publish commit

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 This is the seventh problem set for Gov 1005: Data.
 
 It is the work of Jack Schroeder and Oliver Cordeiro.
+
+Some notes:
+
+(1) Given all the slack polling is getting these days with failing to properly sample and weigh their respsective populations, we were expecting a strong correlation between the two variables. Instead, the correlation between margin of error and outlier percentage was -0.1437853. This is not statistically significant, and shows that the model was largely correct in weighing its respondents.
+
+(2) The correlation among Democratic flips was 0.05736348. This implies that The Upshot was very precise in weighing outliers in districts that actually flipped.
+
+(3) The correlation among Republican holds was -0.2637483. This is a very weak correlation, but it demonstrates that the largest fault in The Upshot's model was over-emphasizing Democrat-leaning outliers in Republican incumbent districts.
+
+(4) There were not enough polled results to calculate meaningful correlations for Republican flips and Democratic holds.
+
+(5) UT-04 is still yet to be officially called, but is looking like a Republican hold. Our data reflect that.
+
+(6) Our outlier percentage is based on all available polls for each district in order to get an accurate tally of Upshot outliers. However, when calculating margin of error, we used only the most recent poll in each district. We did this because within each district, the percentage of outliers is not dependent on time, but the forecasted margin of error is.
+
+(7) The datasets for flips and results were updated for newly called races, but the vote totals were left as they were as of November 9th.
--- ps_7/app.R
+++ ps_7/app.R
@@ -10,7 +10,7 @@ ps7 <- readRDS("ps7.rds")
 ui <- fluidPage(
    
    #We make an application title,
-   titlePanel("Upshot Correctly Weighed Poll Results"),
+   titlePanel("The Upshot Correctly Weighed Poll Results"),
    
    #We make a sidebar that selects based on seat status. 
    sidebarLayout(
@@ -25,19 +25,18 @@ ui <- fluidPage(
         p("We set out to judge how accurate The Upshot was in calculating final weight in its polling.
           We did this by finding the correlation between margin of error (forecasted Democratic
         advantage minus actual Democratic advantage) and the percentage of outliers in each poll
-        (respondents with a high final_weight). Given all the slack polling is getting these days
-        with failing to properly sample and weigh their respsective populations, we were expecting
-        a strong correlation between the two variables. Instead, as a whole, we found no correlation."),
+        (respondents with a high final_weight)."),
         br(),
         plotOutput("myPlot"),
         br(),
-        p("Two notes:"),
-        p("(1) UT-04 is still yet to be officially called, but is looking like a Republican hold. Our
-          data reflect that."),
-        p("(2) Our outlier percentage is based on all available polls for each district in order to get
-          an accurate tally of Upshot outliers. However, when calculating margin of error, we used
-          only the most recent poll in each district. We did this because within each district, the
-          percentage of outliers is not dependent on time, but the forecasted margin of error is.")
+        h4("Correlation Results:"),
+        p("(1) Given all the slack polling is getting these days with failing to properly sample and weigh their respsective populations, we were expecting
+                a strong correlation between the two variables. Instead, the correlation between margin of error and outlier percentage was -0.1437853. This is not
+                statistically significant, and shows that the model was largely correct in weighing its respondents."),
+        p("(2) The correlation among Democratic flips was 0.05736348. This implies that The Upshot was very precise in weighing outliers
+                in districts that actually flipped."),
+        p("(3) The correlation among Republican holds was -0.2637483. This is a very weak correlation, but it demonstrates that the largest fault in
+                The Upshot's model was over-emphasizing Democrat-leaning outliers in Republican incumbent districts.")
         
       )
    )
