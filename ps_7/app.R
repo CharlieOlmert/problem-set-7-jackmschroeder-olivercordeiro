8ab7fc8e563eccf4caed5a448b168b70fddf7f98
Jack Schroeder <jackschroeder@The-Leviathan.local>
2018-11-18 23:51:29 -0500

    updated app

--- ps_7/app.R
+++ ps_7/app.R
@@ -1,24 +1,18 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#We load in the necessary libraries.
 library(shiny)
 library(tidyverse)
 library(readr)
+
+#We read in the RDS file.
 ps7 <- readRDS("ps7.rds")
 
-# Define UI for application that draws a histogram
+#Defining the UI.
 ui <- fluidPage(
    
-   # Application title
-   titlePanel("Deez nutz"),
+   #We make an application title,
+   titlePanel("Upshot Correctly Weighed Poll Results"),
    
-   # Sidebar with a select input for seat flip 
+   #We make a sidebar that selects based on seat status. 
    sidebarLayout(
       sidebarPanel(
          selectInput("flip",
@@ -28,39 +22,48 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution. Include description text. 
       mainPanel(
-         plotOutput("myPlot"), 
-         "We set out to judge how accurate The Upshot was in calculating final_weight 
-          We did this by finding the correlation between margin of error (forecasted 
-         Democratic advantage minus actual Democratic advantage) and the percentage 
-         of outliers in each poll (respondents with a high final_weight). Given all 
-         the slack polling is getting these days with failing to properly sample and 
-         weigh their respective populations, we were expecting a strong correlation 
-         between the two variables. Instead, we found no correlation. We are currently 
-         at work on making a Shiny app displaying these results. Do we need to do any 
-         more work with the data, or is our conclusion interesting enough to stand on 
-         its own? While the headline 'Upshot Correctly Weighed Results' isn't attractive 
-         on its own, given most people distrust polling these days, I think our findings 
-         are rather significant."
+        p("We set out to judge how accurate The Upshot was in calculating final weight in its polling.
+          We did this by finding the correlation between margin of error (forecasted Democratic
+        advantage minus actual Democratic advantage) and the percentage of outliers in each poll
+        (respondents with a high final_weight). Given all the slack polling is getting these days
+        with failing to properly sample and weigh their respsective populations, we were expecting
+        a strong correlation between the two variables. Instead, as a whole, we found no correlation."),
+        br(),
+        plotOutput("myPlot"),
+        br(),
+        p("Two notes:"),
+        p("(1) UT-04 is still yet to be officially called, but is looking like a Republican hold. Our
+          data reflect that."),
+        p("(2) Our outlier percentage is based on all available polls for each district in order to get
+          an accurate tally of Upshot outliers. However, when calculating margin of error, we used
+          only the most recent poll in each district. We did this because within each district, the
+          percentage of outliers is not dependent on time, but the forecasted margin of error is.")
+        
       )
    )
 )
 
-# Define server logic required to draw a histogram
+#We make the server logic necessary to make the output plot.
 server <- function(input, output) {
    
    output$myPlot <- renderPlot({
+#We use ps7 and filter by seat status to make our graph.
     ps7 %>% 
        filter(flip %in% input$flip) %>% 
+#We make a scatter plot using margin of error and outlier percentage (with party as color).
        ggplot(aes(x = margin_of_error, y = outlier_per, color = win_party)) +
        geom_point() +
+#This regression line changes color according to which party it tracks.
        geom_smooth(method = "lm") +
+#We also add graph and axis titles.
        ggtitle("Margin of Error vs. Outlier Percentage in Upshot Polling", subtitle = "Sorted by Seat Status, Colored by Winning Party") +
        xlab("Margin of Error") +
        ylab("Outlier Percentage") +
+#This code assigns colors to our D and R values.
        scale_color_manual(values = c(D = "blue", R = "red"))
    })
 }
 
-# Run the application 
+#We run the application. 
 shinyApp(ui = ui, server = server)
 
